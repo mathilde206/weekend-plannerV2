d77c149a61a0ffa928c44324dbef1914acfaa3ba
Mathilde Barbier <mathilde.barbier@ppro.com>
2018-07-06 21:17:31 +0200

    Add the account app in django

---
+++ src/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    UserRetrieveAPIView,
+    UserCreateApiView,
+)
+
+urlpatterns = [
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='getUser'),
+    path('register/', UserCreateApiView.as_view(), name='register')
+    # path(r'^register/$', UserCreateApiView.as_view(), name='register'),
+    # path(r'^login/$', UserLoginApiView.as_view(), name='login'),
+]
